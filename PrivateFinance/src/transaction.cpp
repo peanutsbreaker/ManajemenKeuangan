925ce13917568ce96497f071ea04f4a198c41c48
SepiAnanda <124240066@student.upnyk.ac.id>
2025-07-24 23:25:33 +0700

    path build dan transaction diperbarui

--- PrivateFinance/src/transaction.cpp
+++ PrivateFinance/src/transaction.cpp
@@ -1,3 +1,14 @@
-#include "C:\Users\LENOVO\Documents\Github\keuangan\ManajemenKeuangan\PrivateFinance\include\transaction.hpp" //merah karena di laptop tidak dalam satu direktori
+#include "C:\Users\LENOVO\Documents\Github\keuangan\ManajemenKeuangan\PrivateFinance\include\Transaction.hpp" //merah karena di setiap laptop beda PATH-nya
 
+Transaction::Transaction(int id, Type type, const std::string &date, double amount, const std::string &category, const std::string &description)
+    : id_(id), type_(type), date_(date), amount_(amount), category_(category), description_(description) {}
+
+int Transaction::getId() const { return id_; }
+Transaction::Type Transaction::getType() const { return type_; }
+const std::string& Transaction::getDate() const { return date_; }
+double Transaction::getAmount() const { return amount_; }
+const std::string& Transaction::getCategory() const { return category_; }
+const std::string& Transaction::getDescription() const { return description_; }
+
+void Transaction::setId(int id) { id_ = id; }
 
